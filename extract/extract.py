bf6dfbcd5e5eeda64861c2ddc005efc9f80d0bfd
aparnac25 <aparnac25@gmail.com>
2021-03-29 18:17:57 -0400

    added code for data frame

--- extract/extract.py
+++ extract/extract.py
@@ -21,11 +21,11 @@ A function to extract GPS coordinates from a PDF and outputting a CSV file with 
     	#parsed_pdf = parser.from_file(pdf_path)    	
     	# saving content of PDF
     	#data = parsed_pdf['content']    	
-    	#return(data)	
-    # maybe combine with above function so end data is pandas DF of coordinates
-	# def extract_gps(data):
+    	# end data is pandas DF of coordinates
+
     # def conversion(X):
     	# geo-pandas package?
+
     # def pdf_plot(Y):
     	# folium package 
 
@@ -37,7 +37,8 @@ import re
 import pandas as pd
 
 def extract(pdf_path):
-    """This function will parse the PDF into a string and from there it will extract 
+    """
+    This function will parse the PDF into a string and from there it will extract 
     out gps coordiantes from the document
     """
     # opening up PDF with tika parser
@@ -50,11 +51,23 @@ def extract(pdf_path):
     cleaned_data = "".join(pdf_data.split())
 
     # pattern for gps coordinates
-    pattern = re.compile('''(\d{1,3}°\d{1,3}′\d{1,3}.\d{1,3}′′S,\d{1,3}°\d{1,3}′\d{1,3}.\d{1,3}′′E)''')  
+    pattern = re.compile('''(\d{1,3}°\d{1,3}′\d{1,3}.\d{1,3}′′[A-Za-z],\d{1,3}°\d{1,3}′\d{1,3}.\d{1,3}′′[A-Za-z])''')  
 
-    # match the pattern to the parsed data .
+    # match the pattern to the parsed data.
     gps_cords = pattern.findall(cleaned_data)
-    print(gps_cords)
+
+    # split list into latitude and longitude
+    split_coords = []
+
+    for elem in gps_coords:
+        lst = elem.split(",")
+        dct = {lst[0], lst[1]}
+        split_coords.append(dct)
+
+    # convert list to data frame 
+    coords_df = pd.DataFrame(split_coords, columns = ["Latitude", "Longitude"])
+    
+    return(coords_df)
 
 
 # asks user to import the path to pdf that they want extract to get the content from (remeber to have .pdf at end of file --add
