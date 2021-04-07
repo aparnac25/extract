3294299a328d72b3716b4a0327aa1618d17f0d3f
aparnac25 <aparnac25@gmail.com>
2021-04-07 19:38:23 -0400

    removed re from install requirements

--- setup.py
+++ setup.py
@@ -8,11 +8,11 @@ from setuptools import setup
 # build command
 setup(
     name="extract",
-    version="0.0.1",
+    version="0.0.2",
     author="Aparna Chandrashekar",
     author_email="achandrashekar@gradcenter.cuny.edu",
     description="program to read PDF and extract coordinates to plot onto map'",
-    install_requires = ["pandas", "tika", "re", "folium"], 
+    install_requires = ["pandas", "tika", "folium"], 
     classifiers=["Programming Language :: Python :: 3"],
     scripts= ['extract/extract.py']
 )
