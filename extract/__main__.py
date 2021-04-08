b0f14f28756779e9437c0e99e7657f29a142485f
aparnac25 <aparnac25@gmail.com>
2021-04-08 15:55:30 -0400

    working main file

---
+++ extract/__main__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+"""
+Command line interface 
+"""
+
+from extract import decimaldegree, extract
+
+if __name__ == "__main__":
+	decimaldegree(degree, minutes, seconds, hemisphere)
+	extract(pdf_path)
+	
